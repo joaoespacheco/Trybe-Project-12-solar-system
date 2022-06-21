2854a9578d7455c287fba13d7fd800b4113cc2f3
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-06-21 18:06:22 -0300

    Conclusão do décimo requisito - Requisito 10/10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+function missionCardGenerator({ name, year, country, destination }) {
+  return (
+    <MissionCard
+      key={ name }
+      name={ name }
+      year={ year }
+      country={ country }
+      destination={ destination }
+    />
+  );
+}
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div>
+          {missions.map((mission) => missionCardGenerator(mission))}
+        </div>
       </div>
     );
   }
